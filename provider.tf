0cbc96a770208ec982221dcd4bee05088c32f8f3
Devssmat <syjusacademy@gmail.com>
2026-01-18 10:43:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,9 @@
-provider "aws" {
-  region = "us-west-2"
+variable "region" {
+    default = "us-west-2"
+}
+variable "vpc_cidr" {
+    default = "50.0.0.0/16"
+}
+variable "name" {
+    default = "ssvpc"
 }
