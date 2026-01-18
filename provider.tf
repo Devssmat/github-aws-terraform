76a326f6151923ef79383daee8fd917bc691e968
Devssmat <syjusacademy@gmail.com>
2026-01-18 10:35:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.28.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "us-west-2" # Oregon
+  region = "us-west-2"
 }
