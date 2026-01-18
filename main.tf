942c88496eac4671c17391c8b33c67b507033817
Devssmat <syjusacademy@gmail.com>
2026-01-18 10:41:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,39 +1,11 @@
-resource "aws_security_group" "web_sg" {
-  name        = "ww-sg"
-  description = "Allow SSH and HTTP"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+provider "aws" {
+   region = var.region
 }
 
-resource "aws_instance" "org" {
-  ami           = "ami-00a8151272c45cd8e"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name      = "slkey"
-
-  tags = {
-    Name        = "web-server"
-    Environment = "dev"
-    Owner       = "DevOps"
-    Project     = "terraform-demo"
-  }
+resource "aws_vpc" "main" {
+   cidr_block = var.vpc_cidr
+   enable_ens_hostnames = true
+   tags = {
+     Name = "${var.name}-vpc"
+   }
 }
